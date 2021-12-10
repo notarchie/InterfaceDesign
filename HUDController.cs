9eded33877e1044cdc14342d9a95e1670fa54c75
archie <archieclarkmcg@gmail.com>
2021-12-10 17:02:46 +0000

    Added code from labs to use to help

---
+++ HUDController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class HUDController : MonoBehaviour
+{
+
+    public TextMeshProUGUI timeText;
+    float startTime;
+    public TextMeshProUGUI countText;
+    private int count;
+    public Image health;
+    public Image collected;
+    public GameObject player;
+
+    void Start()
+    {
+        startTime = Time.time;
+        collected.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString("00");
+        string seconds = (t % 60).ToString("00");
+        timeText.text = minutes + ":" + seconds;
+
+        health.fillAmount -= +0.02f * Time.deltaTime;
+
+        if(collected >= 1)
+        {
+            collected.gameObject.SetActive(true);
+        }
+
+
+        if(health.fillAmount == 0f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+    }
+
+    public void incrementCount()
+    {
+        count++;
+        countText.text = count.ToString();
+    }
+
+    public void ReduceHealth()
+    {
+        health.fillAmount -= 0.1f;
+    }
+
+    public void BallSize(float sliderval)
+    {
+        player.transform.localScale = new Vector3(sliderval, sliderval, sliderval);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public float speed;
+	public Rigidbody rb;
+    public HUDController HUD;
+    
+
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp")
+        {
+			other.gameObject.SetActive (false);
+            HUD.incrementCount();
+        }
+        else if (other.gameObject.tag == "DontPickUp")
+        {
+			other.gameObject.SetActive (false);
+            HUD.ReduceHealth();
+		}
+	}
+}
